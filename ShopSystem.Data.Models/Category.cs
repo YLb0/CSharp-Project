c4d513fc8d3ea2bc70e87a9e718d0bda580916de
YLb0 <ylber.djemali@abv.bg>
2023-07-27 23:12:03 +0200

    Add Property
    Add Property For Product and Category Entity

--- ShopSystem.Data.Models/Category.cs
+++ ShopSystem.Data.Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace ShopSystem.Data.Models
 {
     public class Category
     {
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public bool IsActive { get; set; } = false;
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
--- ShopSystem.Data.Models/Product.cs
+++ ShopSystem.Data.Models/Product.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShopSystem.Data.Models
 {
     public class Product
     {
+        public Product()
+        {
+            Id = Guid.NewGuid();
+        }
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ImageUrl { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Category { get; set; } = null!;
+
+        [Required]
+        public bool IsActive { get; set; } = false;
     }
 }
