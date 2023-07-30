78450da6bb181fd77358cae16034f539b6399f6c
YLb0 <ylber.djemali@abv.bg>
2023-07-31 00:14:45 +0200

    Added   ProductService

---
+++ ShopSystems.Web.ViewModels/Category/AllCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopSystems.Web.ViewModels.Category
+{
+    public class AllCategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public bool IsActive { get; set; } = false;
+    }
+}
---
+++ ShopSystems.Web.ViewModels/Product/AddProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ShopSystems.Web.ViewModels.Category;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopSystems.Web.ViewModels.Product
+{
+    public class AddProductViewModel
+    {
+        [Required]
+        [StringLength(60)]
+        public string Name { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [StringLength(300, MinimumLength = 2)]
+        public string Description { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+
+        public IEnumerable<AllCategoryViewModel> Categories { get; set; } = new List<AllCategoryViewModel>();
+    }
+}
