0d59d425eabf1d4ff94bc76892e27cecae3f024a
YLb0 <ylber.djemali@abv.bg>
2023-07-31 18:41:26 +0200

    Added CategoryController

--- CSsharp-Web-Project/Controllers/CategoryController.cs
+++ CSsharp-Web-Project/Controllers/CategoryController.cs
@@ -1,86 +1,86 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using ProductSystem.Services.Data.Interfaces;
-//using ShopSystems.Web.ViewModels.Category;
-//using ShopSystems.Web.ViewModels.Product;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductSystem.Services.Data.Interfaces;
+using ShopSystems.Web.ViewModels.Category;
+using ShopSystems.Web.ViewModels.Product;
 
-//namespace CSsharp_Web_Project.Controllers
-//{
-//    public class CategoryController : Controller
-//    {
-//        private readonly ICategoryService categoryService;
+namespace CSsharp_Web_Project.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
 
-//        public CategoryController(ICategoryService categoryService)
-//        {
-//            this.categoryService = categoryService;
-//        }
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
 
-//        public async Task<IActionResult> All()
-//        {
-//            var model = await categoryService.GetAllCategoryAsync();
+        public async Task<IActionResult> All()
+        {
+            var model = await categoryService.GetAllCategoryAsync();
 
-//            return View(model);
-//        }
+            return View(model);
+        }
 
-//        [HttpGet]
-//        public IActionResult Add()
-//        {
-//            AddCategoryViewModel model = new AddCategoryViewModel();
+        [HttpGet]
+        public IActionResult Add()
+        {
+            AddCategoryViewModel model = new AddCategoryViewModel();
 
-//            return View(model);
-//        }
+            return View(model);
+        }
 
-//        [HttpPost]
-//        public async Task<IActionResult> Add(AddCategoryViewModel model)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return View(model);
-//            }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
 
-//            await categoryService.AddCategoryAsync(model);
+            await categoryService.AddCategoryAsync(model);
 
-//            return RedirectToAction(nameof(All));
-//        }
+            return RedirectToAction(nameof(All));
+        }
 
-//        [HttpPost]
-//        public async Task<IActionResult> Delete(string id, ProductPreDeleteViewModel model)
-//        {
-//            try
-//            {
-//                await categoryService.DeleteCategoryAsync(id);
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id, ProductPreDeleteViewModel model)
+        {
+            try
+            {
+                await categoryService.DeleteCategoryAsync(id);
 
-//                return RedirectToAction(nameof(All));
-//            }
-//            catch (Exception)
-//            {
-//                return RedirectToAction(nameof(All));
-//            }
+                return RedirectToAction(nameof(All));
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(All));
+            }
 
-//        }
+        }
 
-//        [HttpGet]
-//        public async Task<IActionResult> Edit(int id)
-//        {
-//            AddCategoryViewModel? category = await categoryService.GetCategoryByIDForEditAsync(id);
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            AddCategoryViewModel? category = await categoryService.GetCategoryByIDForEditAsync(id);
 
-//            if (category == null)
-//            {
-//                return RedirectToAction(nameof(All));
-//            }
+            if (category == null)
+            {
+                return RedirectToAction(nameof(All));
+            }
 
-//            return View(category);
-//        }
-//        [HttpPost]
-//        public async Task<IActionResult> Edit(int id, AddCategoryViewModel model)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return View(model);
-//            }
+            return View(category);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, AddCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
 
-//            await categoryService.EditCategoryAsync(model, id);
+            await categoryService.EditCategoryAsync(model, id);
 
-//            return RedirectToAction(nameof(All));
-//        }
-//    }
-//}
+            return RedirectToAction(nameof(All));
+        }
+    }
+}
